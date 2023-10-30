87b617da8fb1ac4a441d7d222e16fa2d12799155
pavanpk1707 <148988297+pavanpk1707@users.noreply.github.com>
2023-10-30 13:55:23 +0000

    Calculator

---
+++ Variables_Functions/calculator.py
@@ -0,0 +1,9 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+print(f"Addition of {x} and {y} is {x+y}")
+print(f"Subtraction of {x} and {y} is {x-y}")
+print(f"Multiplication of {x} and {y} is {x*y}")
+print(f"Division of {x} and {y} is {round(x/y,2)}")
+print(f"Modulo of {x} and {y} is {x % y}")
+print(f"power of {x} and {y} is {x ** y}")
